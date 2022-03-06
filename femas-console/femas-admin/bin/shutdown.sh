c34f06c8573110e79850c1a02e27766eecdcea98
zilongTong <flamingo.ge.895@gmail.com>
2022-03-06 15:06:38 +0800

    verify femas example config

--- femas-console/femas-admin/bin/shutdown.sh
+++ femas-console/femas-admin/bin/shutdown.sh
@@ -2,13 +2,10 @@
 
 pid=`ps ax | grep -i 'femas-admin' |grep java | grep -v grep | awk '{print $1}'`
 if [ -z "$pid" ] ; then
-        echo "No femas-admin running."
+        echo "No femas-admin process found."
         exit -1;
 fi
 
-echo "The femas-admin(${pid}) is running..."
-
 kill ${pid}
 
-
-echo "Send shutdown request to femas-admin(${pid}) OK"
+echo "Send shutdown request to femas-admin(${pid}) success."
