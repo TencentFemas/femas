ac522c435e8f5c90ab2481fbdb89ef48113cf06a
jolyonzheng <>
2022-03-07 21:11:55 +0800

    调整admin(启动/停止)脚本

--- femas-console/femas-admin/bin/shutdown.sh
+++ femas-console/femas-admin/bin/shutdown.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 pid=`ps ax | grep -i 'femas-admin' |grep java | grep -v grep | awk '{print $1}'`
-if [ -z "$pid" ] ; then
-        echo "No femas-admin process found."
-        exit -1;
+if [ -z "$pid" ]; then
+  echo "No femas-admin process found."
+  exit -1;
 fi
 
-kill ${pid}
+echo "The femas-admin($pid) is running..."
 
-echo "Send shutdown request to femas-admin(${pid}) success."
+kill $pid
+
+echo "Send shutdown request to femas-admin($pid) OK"
