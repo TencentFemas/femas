e4024ccdc5991154eb5b66aedf17baccb418643b
jolyon <7927558@qq.com>
2022-03-24 22:02:48 +0800

    bug fix: scg governance

--- femas-extensions/femas-extension-springcloud/femas-extension-springcloud-greenwich-gateway/src/main/java/com/tencent/tsf/femas/springcloud/gateway/filter/GateWayHeaderUtils.java
+++ femas-extensions/femas-extension-springcloud/femas-extension-springcloud-greenwich-gateway/src/main/java/com/tencent/tsf/femas/springcloud/gateway/filter/GateWayHeaderUtils.java
@@ -14,15 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.tencent.tsf.femas.springcloud.gateway.filter;
 
 import com.tencent.tsf.femas.common.context.Context;
 import com.tencent.tsf.femas.common.context.ContextConstant;
 import com.tencent.tsf.femas.common.context.factory.ContextFactory;
 import com.tencent.tsf.femas.common.header.AbstractRequestMetaUtils;
+import com.tencent.tsf.femas.common.util.StringUtils;
+import com.tencent.tsf.femas.governance.lane.LaneService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +38,7 @@ import java.util.function.Consumer;
  * @Author leoziltong
  * @Date: 2021/12/28 15:55
  */
-public class GateWayHeaderUtils extends AbstractRequestMetaUtils {
+public class GatewayHeaderUtils extends AbstractRequestMetaUtils {
 
     private volatile ContextConstant contextConstant = ContextFactory.getContextConstantInstance();
 
@@ -41,7 +46,7 @@ public class GateWayHeaderUtils extends AbstractRequestMetaUtils {
 
     private ServerHttpRequest serverHttpRequest;
 
-    public GateWayHeaderUtils(ServerHttpRequest serverHttpRequest) {
+    public GatewayHeaderUtils(ServerHttpRequest serverHttpRequest) {
         this.serverHttpRequest = serverHttpRequest;
         this.builder = serverHttpRequest.mutate();
     }
