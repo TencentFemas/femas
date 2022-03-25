133c33a5afd2db0ec5a4e914afb171f27b14e044
MentosL <1367654518@qq.com>
2022-03-25 15:42:58 +0800

    统一解决项目内线程上下文变量传递入口
    统一解决项目内线程上下文变量传递入口。
    问题点来源于：com.tencent.tsf.femas.springcloud.gateway.loadbalancer.FemasRouteLoadBalancer中processInstanceResponse方法TODO

---
+++ femas-common/src/main/java/com/tencent/tsf/femas/common/util/thread/FemasThreadPoolExecutor.java
@@ -0,0 +1,32 @@
+package com.tencent.tsf.femas.common.util.thread;
+
+import java.util.concurrent.*;
+
+/**
+ * @author MentosL
+ * @version 1.0
+ * @date 2022/3/25 1:58 PM
+ */
+public class FemasThreadPoolExecutor extends ThreadPoolExecutor {
+
+    public FemasThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public FemasThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public FemasThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public FemasThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        super.execute(new FemasRunnale(command));
+    }
+}
