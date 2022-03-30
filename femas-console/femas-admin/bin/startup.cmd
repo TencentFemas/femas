aa37d2ef2b8edb480f57669954b6aa783eb8e225
zhangsheng <773935581@qq.com>
2022-03-30 22:19:32 +0800

    add config and external db option

--- femas-console/femas-admin/bin/startup.cmd
+++ femas-console/femas-admin/bin/startup.cmd
@@ -15,8 +15,21 @@ set CUSTOM_SEARCH_LOCATIONS=file:%BASE_DIR%\conf
 
 set "FAMES_JVM_OPTS=-server -Xms2g -Xmx2g -Xmn1g -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=%BASE_DIR%\logs\java_heapdump.hprof -XX:-UseLargePages"
 
-set "JAVA_OPTS=%JAVA_OPTS% -Dspring.config.location=%BASE_DIR%/conf/bootstrap.yaml -jar %BASE_DIR%\femas-admin.jar"
 
+set dbType=%1
+rem 判断是否传入
+if not defined dbType set dbType=inner
+if %dbType%==external set %dbType%=external
+if %dbType%==inner (
+    set JAVA_OPTS=-Dspring.config.location=%BASE_DIR%\conf\bootstrap.yaml -jar %BASE_DIR%\femas-admin.jar
+)
+if %dbType%==external (
+    set JAVA_OPTS=-DdbType=external -Dspring.config.location=%BASE_DIR%\conf\bootstrap.yaml -jar %BASE_DIR%\femas-admin.jar
+)
+rem 传入非external
+if %dbType% neq external (
+    set JAVA_OPTS=-Dspring.config.location=%BASE_DIR%\conf\bootstrap.yaml -jar %BASE_DIR%\femas-admin.jar
+)
 set "FAMES_CONFIG_OPTS=--spring.config.additional-location=%CUSTOM_SEARCH_LOCATIONS%"
 
 set "FAMES_LOG4J_OPTS=--logging.config=%BASE_DIR%\conf\logback-spring.xml"
