7881868dd8d31c950ff870aad990c249fde3f3c0
huyuanxin <huyuanxin1999@outlook.com>
2022-05-18 09:52:28 +0800

    feat(RegistryBuilder) add Javadoc and add generics

--- femas-registry/src/main/java/com/tencent/tsf/femas/common/RegistryBuilder.java
+++ femas-registry/src/main/java/com/tencent/tsf/femas/common/RegistryBuilder.java
@@ -1,6 +1,7 @@
 package com.tencent.tsf.femas.common;
 
 import com.tencent.tsf.femas.common.exception.FemasRegisterDescribeException;
+
 import java.util.function.Supplier;
 
 /**
@@ -11,15 +12,45 @@ import java.util.function.Supplier;
  */
 public interface RegistryBuilder<T> {
 
-    T describeClient(Supplier<String> serverAddressSupplier, String namespace, boolean certificate, Object... var) throws FemasRegisterDescribeException;
+    /**
+     * 构建client
+     *
+     * @param serverAddressSupplier 注册地址Supplier
+     * @param namespace             命名空间
+     * @param certificate           true-调用{@link #certificateClient(Supplier, String, Object...)},false调用{{@link #build(Supplier, String)}}
+     * @param args                  参数-调用{@link #certificateClient(Supplier, String, Object...)}的参数
+     * @return client
+     * @throws FemasRegisterDescribeException
+     */
+    T describeClient(Supplier<String> serverAddressSupplier, String namespace, boolean certificate, Object... args) throws FemasRegisterDescribeException;
 
+    /**
+     * 构建client,与certificateClient的区别是不带参数args
+     *
+     * @param serverAddressSupplier 注册地址Supplier
+     * @param namespace             命名空间
+     * @return client
+     */
     T build(Supplier<String> serverAddressSupplier, String namespace);
 
-    T certificateClient(Supplier serverAddressSupplier, String namespace, Object... var);
+    /**
+     * 获得认证的client
+     *
+     * @param serverAddressSupplier 注册地址Supplier
+     * @param namespace             命名空间
+     * @param args                  参数
+     * @return 认证的Client
+     */
+    T certificateClient(Supplier<String> serverAddressSupplier, String namespace, Object... args);
 
+    /**
+     * 获得注册地址
+     *
+     * @param serverAddressSupplier 注册地址Supplier
+     * @return 注册地址
+     */
     default String getKey(Supplier<String> serverAddressSupplier) {
-        String key = serverAddressSupplier.get();
-        return key;
+        return serverAddressSupplier.get();
     }
 
 }
