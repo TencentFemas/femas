0dc2540cc458101cf566c94a11c889fcbcea6db9
huyuanxin <huyuanxin1999@outlook.com>
2022-06-07 16:37:48 +0800

    test(nacos-registry-test) add nacos registry test

--- femas-registry-impl/femas-registry-nacos/src/test/java/com/tencent/tsf/femas/registry/nacos/NacosRegistryTest.java
+++ femas-registry-impl/femas-registry-nacos/src/test/java/com/tencent/tsf/femas/registry/nacos/NacosRegistryTest.java
@@ -16,15 +16,109 @@
  */
 package com.tencent.tsf.femas.registry.nacos;
 
+import com.tencent.tsf.femas.common.RegistryConstants;
+import com.tencent.tsf.femas.common.discovery.AbstractServiceDiscoveryClient;
+import com.tencent.tsf.femas.common.entity.EndpointStatus;
+import com.tencent.tsf.femas.common.entity.Service;
+import com.tencent.tsf.femas.common.entity.ServiceInstance;
+import com.tencent.tsf.femas.common.serviceregistry.AbstractServiceRegistry;
+import com.tencent.tsf.femas.registry.impl.nacos.discovery.NacosServiceDiscoveryClient;
+import com.tencent.tsf.femas.registry.impl.nacos.serviceregistry.NacosServiceRegistry;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * @Author leoziltong@tencent.com
- * @Date: 2022/5/19 19:14
+ * @author huyuanxin
  */
 public class NacosRegistryTest {
-    @Test
-    public void test(){
 
+    private Map<String, String> configMap;
+
+    private AbstractServiceRegistry abstractServiceRegistry;
+
+    private Service getService() {
+        return new Service("registry-test", "registry-test");
     }
+
+    private ServiceInstance getServiceInstance(String id, Integer port) {
+        return new ServiceInstance(getService(),
+                id,
+                "127.0.0.1",
+                port,
+                System.currentTimeMillis(),
+                "1.0.0",
+                "1.0.0",
+                EndpointStatus.UP,
+                "",
+                new HashMap<>(),
+                new HashMap<>(),
+                true,
+                5
+        );
+    }
+
+    /**
+     * 每次new一个新的原因是,robin轮询刷新需要30s,new 一次进行单测效率更高
+     *
+     * @return 新的EtcdServiceDiscoveryClient
+     */
+    private AbstractServiceDiscoveryClient getDiscoveryClient() {
+        return new NacosServiceDiscoveryClient(configMap);
+    }
+
+    @Before
+    public void init() {
+        configMap = new HashMap<>(3);
+        configMap.put(RegistryConstants.REGISTRY_HOST, "127.0.0.1");
+        configMap.put(RegistryConstants.REGISTRY_PORT, "8848");
+        abstractServiceRegistry = new NacosServiceRegistry(configMap);
+    }
+
+    @Test
+    public void testRegistryAndDeregistry() throws InterruptedException {
+        abstractServiceRegistry.register(getServiceInstance("registry-test-1", 8080));
+        AbstractServiceDiscoveryClient discoveryClient = getDiscoveryClient();
+        Thread.sleep(3000L);
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances(getService());
+        Assert.assertEquals(1, serviceInstanceList.size());
+        abstractServiceRegistry.deregister(getServiceInstance("registry-test-1", 8080));
+        Thread.sleep(3000L);
+        discoveryClient = getDiscoveryClient();
+        serviceInstanceList = discoveryClient.getInstances(getService());
+        Assert.assertEquals(0, serviceInstanceList.size());
+    }
+
+    @Test
+    public void testRegistryAndDeregistryMany() throws InterruptedException {
+        // 注册第一个
+        abstractServiceRegistry.register(getServiceInstance("registry-test-1", 8080));
+        Thread.sleep(3000L);
+        AbstractServiceDiscoveryClient discoveryClient = getDiscoveryClient();
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances(getService());
+        Assert.assertEquals(1, serviceInstanceList.size());
+        // 注册第二个
+        abstractServiceRegistry.register(getServiceInstance("registry-test-2", 8081));
+        Thread.sleep(3000L);
+        discoveryClient = getDiscoveryClient();
+        serviceInstanceList = discoveryClient.getInstances(getService());
+        Assert.assertEquals(2, serviceInstanceList.size());
+        // 反注册一个
+        abstractServiceRegistry.deregister(getServiceInstance("registry-test-1", 8080));
+        Thread.sleep(3000L);
+        discoveryClient = getDiscoveryClient();
+        serviceInstanceList = discoveryClient.getInstances(getService());
+        Assert.assertEquals(1, serviceInstanceList.size());
+        // 反注册第二个
+        abstractServiceRegistry.deregister(getServiceInstance("registry-test-2", 8081));
+        Thread.sleep(3000L);
+        discoveryClient = getDiscoveryClient();
+        serviceInstanceList = discoveryClient.getInstances(getService());
+        Assert.assertEquals(0, serviceInstanceList.size());
+    }
+
 }
