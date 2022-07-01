1288e81881472d349dc127572512d24722afb7d0
huyuanxin <huyuanxin1999@outlook.com>
2022-07-01 16:05:40 +0800

    feat(example): add discovery starter example

---
+++ femas-example/femas-example-springcloud-discovery/src/main/java/com/tencent/tsf/femas/examples/FemasExampleSpringcloudDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.tencent.tsf.femas.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemasExampleSpringcloudDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FemasExampleSpringcloudDiscoveryApplication.class, args);
+    }
+
+}
