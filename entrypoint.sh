1b47dc249dd9702f9c31910de80a1719cf51f544
zilongTong <flamingo.ge.895@gmail.com>
2022-07-25 11:48:51 +0800

    add femas agent bom dependency & refactor greenwich plugins

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-cd /usr/local/src/femas 
+cd /usr/local/src/femas
 BOOTSTRAP=$(find /usr/local/src/femas -regex ".*/femas-admin-starter-.*/startup.sh$")
 bash ${BOOTSTRAP} 
-tail -f $(dirname ${BOOTSTRAP})/../logs/*.out
+tail -f $(dirname ${BOOTSTRAP})/../logs/*.log
